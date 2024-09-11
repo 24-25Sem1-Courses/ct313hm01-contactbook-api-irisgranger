ec20d1cd4f3ef6200d99aebf1f85d633aeff93bf
irisgranger <ngpthuy202@gmail.com>
2024-09-11 09:40:32 +0700

    Define routes for managing contacts

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const contactsController = require('./src/controllers/contacts.controller.js');
+const contactsController = require('../controllers/contacts.controller.js');
 
 const router = express.Router();
 
