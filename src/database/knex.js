19382cbb8c447c131b4f260d283a7e237c271c8c
irisgranger <ngpthuy202@gmail.com>
2024-11-12 02:42:01 +0700

    pg

--- src/database/knex.js
+++ src/database/knex.js
@@ -1,7 +1,9 @@
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 module.exports = require('knex')({
-  client: 'mysql',
+  // client: 'mysql', local
+  client: 'pg', //deploy
+
   connection: {
     host: DB_HOST,
     port: DB_PORT,
